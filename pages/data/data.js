6d767785c0449ded16c12723e7c442c3328ae306
PIPI <13864275912@163.com>
2025-12-17 19:01:57 +0800

    箭头函数的学习

--- pages/data/data.js
+++ pages/data/data.js
@@ -4,14 +4,20 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+   name:"张三"
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log("index的onlad触发");
+
+    setTimeout(() => {
+      this.setData({
+        name:"厉色"
+      })
+    }, 2000);
+    
   },
 
   /**
